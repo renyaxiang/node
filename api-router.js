a23ba9a2c0e02c6de40d7d04df74bbf216b6d277
xiangrenya <xiangrenya@gmail.com>
2018-01-22 09:47:07 +0800

    delete something unuseful

--- api-router.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var post = require('./api/v1/post')
-
-router.get('/posts', post.getPosts);
-
-module.exports = router;
--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-var Person = require('../app/controllers/person');
-
-module.exports = function (app) {
-
-    app.post('/person/add', Person.add);
-    app.get('/person/list', Person.list);
-    app.delete('/person/del', Person.del);
-};
