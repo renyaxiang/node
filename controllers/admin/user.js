5455ddd6501a351c6c88b0614d165ea558e79950
xiangrenya <xiangrenya@gmail.com>
2018-03-01 20:56:20 +0800

    update

--- controllers/admin/user.js
+++ controllers/admin/user.js
@@ -7,32 +7,34 @@ const config = require('../../config')
 
 // 查询列表：分页、模糊查询
 exports.getList = function (req, res, next) {
-    let { username = '', page = 1, perPage = 20 } = req.query
-    page = parseInt(page)
-    perPage = parseInt(perPage)
+  let {
+    username = '', page = 1, perPage = 10
+  } = req.query
+  page = parseInt(page)
+  perPage = parseInt(perPage)
 
-    const usersPromise = new Promise((resolve, reject) => {
-        UserService.getList(page, perPage, username).then(datas => {
-            resolve(datas)
-        })
+  const usersPromise = new Promise((resolve, reject) => {
+    UserService.getList(page, perPage, username).then(datas => {
+      resolve(datas)
     })
+  })
 
-    const countPromise = new Promise((resolve, reject) => {
-        UserService.count(username).then(data => {
-            resolve(data)
-        })
+  const countPromise = new Promise((resolve, reject) => {
+    UserService.count(username).then(data => {
+      resolve(data)
     })
+  })
 
-    Promise.all([usersPromise, countPromise]).then(([users, count]) => {
-        res.render('admin/user', {
-            title: '用户列表',
-            users: users,
-            currentPage: page,
-            perPage: perPage,
-            pages: Math.ceil(count/10),
-            key: key
-        })
-    }).catch(err => {
-        next(err)
+  Promise.all([usersPromise, countPromise]).then(([users, count]) => {
+    res.render('admin/user', {
+      title: '用户列表',
+      users: users,
+      currentPage: page,
+      perPage: perPage,
+      pages: Math.ceil(count / perPage),
+      key: key
     })
+  }).catch(err => {
+    next(err)
+  })
 }
